77927deb9641a8d493a9a7da01187d1ce1a3b0dc
Scherrit Knoesen <scherritknoesen@capitecbank.co.za>
2022-08-18 04:41:49 +0200

    module 1

---
+++ ssg/site.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+# import os
+
+class Site():
+
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+
+
+# test = Site("C:\temp\source", "C:\temp\test")
+# test.create_dir("C:\temp\another")
+
+
+
